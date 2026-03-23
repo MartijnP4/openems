420f26400d054a16140f6de90f09ff8682b80495
MartijnP4 <martijn.poppe@gmail.com>
2026-03-23 13:45:39 +0100

    Create BatteryInverterConfig.java

---
+++ io.openems.edge.batteryinverter.deye/src/io/openems/edge/batteryinverter/deye/BatteryInverterConfig.java
@@ -0,0 +1,28 @@
+package io.openems.edge.batteryinverter.deye;
+
+import org.osgi.service.metatype.annotations.AttributeDefinition;
+import org.osgi.service.metatype.annotations.ObjectClassDefinition;
+
+@ObjectClassDefinition(
+    name = "BatteryInverter Deye SG04LP3",
+    description = "BatteryInverter component for Deye SUN-10K SG04LP3-EU"
+)
+@interface BatteryInverterConfig {
+
+    String service_pid();
+
+    @AttributeDefinition(name = "Component-ID", description = "Unique ID of this Component")
+    String id() default "batteryInverter0";
+
+    @AttributeDefinition(name = "Alias", description = "Human-readable name")
+    String alias() default "";
+
+    @AttributeDefinition(name = "Is enabled?")
+    boolean enabled() default true;
+
+    @AttributeDefinition(name = "Modbus-ID", description = "ID of Modbus bridge")
+    String Modbus_target() default "(enabled=true)";
+
+    @AttributeDefinition(name = "Modbus Unit-ID", description = "Deye slave ID (default 1)")
+    int modbusUnitId() default 1;
+}
