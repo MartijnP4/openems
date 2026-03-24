ef2dfd929ee7f4bb7679b74d23f2138fcd2abad6
MartijnP4 <martijn.poppe@gmail.com>
2026-03-24 13:18:23 +0100

    Update BatteryInverterConfig.java

--- io.openems.edge.batteryinverter.deye/src/io/openems/edge/batteryinverter/deye/BatteryInverterConfig.java
+++ io.openems.edge.batteryinverter.deye/src/io/openems/edge/batteryinverter/deye/BatteryInverterConfig.java
@@ -19,7 +19,7 @@ import org.osgi.service.metatype.annotations.ObjectClassDefinition;
     boolean enabled() default true;
 
     @AttributeDefinition(name = "Modbus-ID", description = "ID of Modbus bridge")
-    String Modbus_target() default "(enabled=true)";
+    String Modbus_target() default "(id=modbus0)";
 
     @AttributeDefinition(name = "Modbus Unit-ID", description = "Deye slave ID (default 1)")
     int modbusUnitId() default 1;
