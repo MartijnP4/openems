04c43f28839d76ac4d23274cc4f0e4e80d1574b0
MartijnP4 <martijn.poppe@gmail.com>
2026-03-25 00:19:48 +0100

    Update BatteryConfig.java

--- io.openems.edge.battery.deye/src/io/openems/edge/battery/deye/BatteryConfig.java
+++ io.openems.edge.battery.deye/src/io/openems/edge/battery/deye/BatteryConfig.java
@@ -19,7 +19,7 @@ import org.osgi.service.metatype.annotations.ObjectClassDefinition;
     boolean enabled() default true;
 
     @AttributeDefinition(name = "Modbus-ID", description = "ID of Modbus bridge")
-    String Modbus_target() default "modbus0";
+    String Modbus_target() default "(id=modbus0)";
 
     @AttributeDefinition(name = "Modbus Unit-ID", description = "Unit ID of Modbus device (default 1)")
     int modbusUnitId() default 1;
